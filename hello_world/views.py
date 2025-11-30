51779aeba28b1781e1b6b536f3de9b3aaaf935a7
MaciejDybowski <MaciejDybowski@users.noreply.github.com>
2025-11-30 17:54:24 +0100

    fix tests

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,8 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-my_name = "Beata"
-msg = "Hello World!"
+my_name = "Maciej"
+msg = "Greetings from the DevOps classses!"
 
 @app.route('/')
 def index():
